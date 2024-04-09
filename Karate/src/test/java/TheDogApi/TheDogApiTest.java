021c59ca62814c8be2772bfda2208d3564fe528e
MaxLeiee22 <moeketsil@qotsolutions.co.za>
2024-04-09 12:11:37 +0200

    runner2

--- Karate/src/test/java/TheDogApi/TheDogApiTest.java
+++ Karate/src/test/java/TheDogApi/TheDogApiTest.java
@@ -17,7 +17,7 @@ class TheDogApiTest {
 
     @Test
     void testParallel() {
-        Results results = Runner.path("classpath:TheDogApi/votes")
+        Results results = Runner.path("classpath:TheDogApi/votes/createVotes.feature")
                 .outputCucumberJson(true)
                 .parallel(5);
         generateReport(results.getReportDir());
